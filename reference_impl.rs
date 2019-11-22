018874638db18150075899d43e1084700178bccb
Jack O'Connor <oconnor663@gmail.com>
2019-11-22 17:47:07 -0500

    update reference_impl.rs again

--- reference_impl.rs
+++ reference_impl.rs
@@ -1,3 +1,6 @@
+use core::cmp::min;
+use core::convert::TryInto;
+
 const OUT_LEN: usize = 32;
 const KEY_LEN: usize = 32;
 const BLOCK_LEN: usize = 64;
@@ -12,8 +15,7 @@ const KEYED_HASH: u32 = 1 << 4;
 const DERIVE_KEY: u32 = 1 << 5;
 
 const IV: [u32; 8] = [
-    0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C,
-    0x1F83D9AB, 0x5BE0CD19,
+    0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19,
 ];
 
 const MSG_SCHEDULE: [[usize; 16]; ROUNDS] = [
@@ -26,28 +28,8 @@ const MSG_SCHEDULE: [[usize; 16]; ROUNDS] = [
     [12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11],
 ];
 
-fn words_from_litte_endian_bytes(bytes: &[u8], words: &mut [u32]) {
-    for (b, w) in bytes.chunks_exact(4).zip(words.iter_mut()) {
-        *w = u32::from_le_bytes(core::convert::TryInto::try_into(b).unwrap());
-    }
-}
-
-fn little_endian_bytes_from_words(words: &[u32], bytes: &mut [u8]) {
-    for (w, b) in words.iter().zip(bytes.chunks_exact_mut(4)) {
-        b.copy_from_slice(&w.to_le_bytes());
-    }
-}
-
 // The mixing function, G, which mixes either a column or a diagonal.
-fn g(
-    state: &mut [u32; 16],
-    a: usize,
-    b: usize,
-    c: usize,
-    d: usize,
-    mx: u32,
-    my: u32,
-) {
+fn g(state: &mut [u32; 16], a: usize, b: usize, c: usize, d: usize, mx: u32, my: u32) {
     state[a] = state[a].wrapping_add(state[b]).wrapping_add(mx);
     state[d] = (state[d] ^ state[a]).rotate_right(16);
     state[c] = state[c].wrapping_add(state[d]);
@@ -71,7 +53,7 @@ fn round(state: &mut [u32; 16], m: &[u32; 16], schedule: &[usize; 16]) {
     g(state, 3, 4, 9, 14, m[schedule[14]], m[schedule[15]]);
 }
 
-fn compress_inner(
+fn compress(
     chaining_value: &[u32; 8],
     block_words: &[u32; 16],
     offset: u64,
@@ -91,44 +73,14 @@ fn compress_inner(
         IV[1],
         IV[2],
         IV[3],
-        (offset as u32),
-        ((offset >> 32) as u32),
+        offset as u32,
+        (offset >> 32) as u32,
         block_len,
         flags,
     ];
     for r in 0..ROUNDS {
         round(&mut state, &block_words, &MSG_SCHEDULE[r]);
     }
-    state
-}
-
-// The standard compression function updates an 8-word chaining value in place.
-fn compress(
-    chaining_value: &mut [u32; 8],
-    block_words: &[u32; 16],
-    offset: u64,
-    block_len: u32,
-    flags: u32,
-) {
-    let state =
-        compress_inner(chaining_value, block_words, offset, block_len, flags);
-    for i in 0..8 {
-        chaining_value[i] = state[i] ^ state[i + 8];
-    }
-}
-
-// The extended compression function returns a new 16-word extended output.
-// Note that the first 8 words of output are the same as with compress().
-// Implementations that do not support extendable output can omit this.
-fn compress_extended(
-    chaining_value: &[u32; 8],
-    block_words: &[u32; 16],
-    offset: u64,
-    block_len: u32,
-    flags: u32,
-) -> [u32; 16] {
-    let mut state =
-        compress_inner(chaining_value, block_words, offset, block_len, flags);
     for i in 0..8 {
         state[i] ^= state[i + 8];
         state[i + 8] ^= chaining_value[i];
@@ -136,8 +88,25 @@ fn compress_extended(
     state
 }
 
-// The output of a chunk or parent node, which could be an 8-word chaining
-// value or any number of root output bytes.
+fn first_8_words(compression_output: [u32; 16]) -> [u32; 8] {
+    compression_output[0..8].try_into().unwrap()
+}
+
+fn words_from_litte_endian_bytes(bytes: &[u8], words: &mut [u32]) {
+    for (bytes_block, word) in bytes.chunks_exact(4).zip(words.iter_mut()) {
+        *word = u32::from_le_bytes(bytes_block.try_into().unwrap());
+    }
+}
+
+fn little_endian_bytes_from_words(words: &[u32], bytes: &mut [u8]) {
+    for (word, bytes_block) in words.iter().zip(bytes.chunks_exact_mut(4)) {
+        bytes_block.copy_from_slice(&word.to_le_bytes());
+    }
+}
+
+// Each chunk or parent node can produce either an 8-word chaining value or, by
+// setting the ROOT flag, any number of final output bytes. The Output struct
+// captures the state just prior to choosing between those two possibilities.
 struct Output {
     input_chaining_value: [u32; 8],
     block_words: [u32; 16],
@@ -148,21 +117,19 @@ struct Output {
 
 impl Output {
     fn chaining_value(&self) -> [u32; 8] {
-        let mut cv = self.input_chaining_value;
-        compress(
-            &mut cv,
+        first_8_words(compress(
+            &self.input_chaining_value,
             &self.block_words,
             self.offset,
             self.block_len,
             self.flags,
-        );
-        cv
+        ))
     }
 
-    fn root_output(&self, out_slice: &mut [u8]) {
+    fn root_output_bytes(&self, out_slice: &mut [u8]) {
         let mut offset = self.offset;
         for out_block in out_slice.chunks_mut(2 * OUT_LEN) {
-            let words = compress_extended(
+            let words = compress(
                 &self.input_chaining_value,
                 &self.block_words,
                 offset,
@@ -213,23 +180,21 @@ impl ChunkState {
             if self.block_len as usize == BLOCK_LEN {
                 let mut block_words = [0; 16];
                 words_from_litte_endian_bytes(&self.block, &mut block_words);
-                let block_flags = self.start_flag() | self.flags;
-                compress(
-                    &mut self.chaining_value,
+                self.chaining_value = first_8_words(compress(
+                    &self.chaining_value,
                     &block_words,
                     self.offset,
                     BLOCK_LEN as u32,
-                    block_flags as u32,
-                );
+                    self.flags | self.start_flag(),
+                ));
                 self.blocks_compressed += 1;
                 self.block = [0; BLOCK_LEN];
                 self.block_len = 0;
             }
 
             let want = BLOCK_LEN - self.block_len as usize;
-            let take = core::cmp::min(want, input.len());
-            self.block[self.block_len as usize..][..take]
-                .copy_from_slice(&input[..take]);
+            let take = min(want, input.len());
+            self.block[self.block_len as usize..][..take].copy_from_slice(&input[..take]);
             self.block_len += take as u8;
             input = &input[take..];
         }
@@ -238,13 +203,12 @@ impl ChunkState {
     fn output(&self) -> Output {
         let mut block_words = [0; 16];
         words_from_litte_endian_bytes(&self.block, &mut block_words);
-        let block_flags = self.flags | self.start_flag() | CHUNK_END;
         Output {
             input_chaining_value: self.chaining_value,
             block_words,
             block_len: self.block_len as u32,
             offset: self.offset,
-            flags: block_flags,
+            flags: self.flags | self.start_flag() | CHUNK_END,
         }
     }
 }
@@ -272,7 +236,7 @@ pub struct Hasher {
     chunk_state: ChunkState,
     key: [u32; 8],
     subtree_stack: [[u32; 8]; 53], // Space for 53 subtree chaining values:
-    num_subtrees: u8,              // 2^53 * CHUNK_LEN = 2^64
+    subtree_stack_len: u8,         // 2^53 * CHUNK_LEN = 2^64
 }
 
 impl Hasher {
@@ -281,7 +245,7 @@ impl Hasher {
             chunk_state: ChunkState::new(key, 0, flags),
             key: *key,
             subtree_stack: [[0; 8]; 53],
-            num_subtrees: 0,
+            subtree_stack_len: 0,
         }
     }
 
@@ -304,33 +268,29 @@ impl Hasher {
         Self::new_internal(&key_words, DERIVE_KEY)
     }
 
-    // Pop the top two subtree chaining values off the stack, compress them
-    // into a parent CV, and put that new CV on top of the stack.
-    fn merge_two_subtrees(&mut self) {
-        let left_child = &self.subtree_stack[self.num_subtrees as usize - 2];
-        let right_child = &self.subtree_stack[self.num_subtrees as usize - 1];
-        let parent_hash = parent_output(
-            left_child,
-            right_child,
-            &self.key,
-            self.chunk_state.flags,
-        )
-        .chaining_value();
-        self.subtree_stack[self.num_subtrees as usize - 2] = parent_hash;
-        self.num_subtrees -= 1;
+    fn push_stack(&mut self, cv: &[u32; 8]) {
+        self.subtree_stack[self.subtree_stack_len as usize] = *cv;
+        self.subtree_stack_len += 1;
     }
 
-    fn push_chunk_chaining_value(&mut self, cv: &[u32; 8], total_bytes: u64) {
-        self.subtree_stack[self.num_subtrees as usize] = *cv;
-        self.num_subtrees += 1;
-        // After pushing the new chunk chaining value onto the stack, assemble
-        // and compress as many parent nodes as we can. The number of 1 bits in
-        // the total number of chunks so far is the same as the number of CVs
-        // that should remain in the stack after this is done.
-        let total_chunks = total_bytes / CHUNK_LEN as u64;
-        while self.num_subtrees as usize > total_chunks.count_ones() as usize {
-            self.merge_two_subtrees();
+    fn pop_stack(&mut self) -> [u32; 8] {
+        self.subtree_stack_len -= 1;
+        self.subtree_stack[self.subtree_stack_len as usize]
+    }
+
+    fn push_chunk_chaining_value(&mut self, mut cv: [u32; 8], total_bytes: u64) {
+        // The new chunk chaining value might complete some subtrees along the
+        // right edge of the growing tree. For each completed subtree, pop its
+        // left child CV off the stack and compress a new parent CV. After as
+        // many parent compressions as possible, push the new CV onto the
+        // stack. The final length of the stack will be the count of 1 bits in
+        // the total number of chunks or (equivalently) input bytes so far.
+        let final_stack_len = total_bytes.count_ones() as u8;
+        while self.subtree_stack_len >= final_stack_len {
+            cv = parent_output(&self.pop_stack(), &cv, &self.key, self.chunk_state.flags)
+                .chaining_value();
         }
+        self.push_stack(&cv);
     }
 
     /// Add input to the hash state. This can be called any number of times.
@@ -338,56 +298,35 @@ impl Hasher {
         while !input.is_empty() {
             if self.chunk_state.len() == CHUNK_LEN {
                 let chunk_cv = self.chunk_state.output().chaining_value();
-                let new_chunk_offset =
-                    self.chunk_state.offset + CHUNK_LEN as u64;
-                self.push_chunk_chaining_value(&chunk_cv, new_chunk_offset);
-                self.chunk_state = ChunkState::new(
-                    &self.key,
-                    new_chunk_offset,
-                    self.chunk_state.flags,
-                );
+                let new_chunk_offset = self.chunk_state.offset + CHUNK_LEN as u64;
+                self.push_chunk_chaining_value(chunk_cv, new_chunk_offset);
+                self.chunk_state =
+                    ChunkState::new(&self.key, new_chunk_offset, self.chunk_state.flags);
             }
 
             let want = CHUNK_LEN - self.chunk_state.len();
-            let take = core::cmp::min(want, input.len());
+            let take = min(want, input.len());
             self.chunk_state.update(&input[..take]);
             input = &input[take..];
         }
     }
 
-    /// Finalize the hash and return the default 32-byte output.
-    pub fn finalize(&self) -> [u8; OUT_LEN] {
-        let mut bytes = [0; OUT_LEN];
-        self.finalize_extended(&mut bytes);
-        bytes
-    }
-
     /// Finalize the hash and write any number of output bytes.
-    pub fn finalize_extended(&self, out_slice: &mut [u8]) {
-        // If the subtree stack is empty, then the current chunk is the root.
-        if self.num_subtrees == 0 {
-            self.chunk_state.output().root_output(out_slice);
-            return;
-        }
-
-        // Otherwise, finalize the current chunk, and then merge all the
-        // subtrees along the right edge of the tree.
-        let mut right_child = self.chunk_state.output().chaining_value();
-        let mut subtrees_remaining = self.num_subtrees as usize;
-        loop {
-            let left_child = &self.subtree_stack[subtrees_remaining - 1];
-            let output = parent_output(
-                left_child,
-                &right_child,
+    pub fn finalize(&self, out_slice: &mut [u8]) {
+        // Starting with the Output from the current chunk, compute all the
+        // parent chaining values along the right edge of the tree, until we
+        // have the root Output.
+        let mut output = self.chunk_state.output();
+        let mut parent_nodes_remaining = self.subtree_stack_len as usize;
+        while parent_nodes_remaining > 0 {
+            parent_nodes_remaining -= 1;
+            output = parent_output(
+                &self.subtree_stack[parent_nodes_remaining],
+                &output.chaining_value(),
                 &self.key,
                 self.chunk_state.flags,
             );
-            if subtrees_remaining == 1 {
-                output.root_output(out_slice);
-                return;
-            }
-            right_child = output.chaining_value();
-            subtrees_remaining -= 1;
         }
+        output.root_output_bytes(out_slice);
     }
 }
