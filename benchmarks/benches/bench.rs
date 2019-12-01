12b6ad2ffa9e743ce3f47d5240935b5109c9249a
Samuel Neves <sneves@dei.uc.pt>
2019-12-01 09:25:21 +0000

    this seems to work for cpb,
    though the codegen from rust/criterion is not optimal. E.g.,
    64-byte blake2s (without the row rotation optimization) runs at
    4.77 cpb natively, while here it reports 5.56 owing to overhead
    from numerous uninlined calls etc.
    
    Criterion's idea of preventing the compiler to optimize things out
    is to volatile read each individual byte of the result:
    
    call    r15 ; blake2b_simd::blake2b::h57be095e3b82d97e
    movzx   eax, [rsp+88h+var_49]
    movzx   eax, [rsp+88h+var_4A]
    movzx   eax, [rsp+88h+var_4B]
    movzx   eax, [rsp+88h+var_4C]
    movzx   eax, [rsp+88h+var_4D]
    movzx   eax, [rsp+88h+var_4E]
    movzx   eax, [rsp+88h+var_4F]
    movzx   eax, [rsp+88h+var_50]
    movzx   eax, [rsp+88h+var_51]
    movzx   eax, [rsp+88h+var_52]
    movzx   eax, [rsp+88h+var_53]
    movzx   eax, [rsp+88h+var_54]
    movzx   eax, [rsp+88h+var_55]
    movzx   eax, [rsp+88h+var_56]
    movzx   eax, [rsp+88h+var_57]
    movzx   eax, [rsp+88h+var_58]
    movzx   eax, [rsp+88h+var_59]
    movzx   eax, [rsp+88h+var_5A]
    movzx   eax, [rsp+88h+var_5B]
    movzx   eax, [rsp+88h+var_5C]
    movzx   eax, [rsp+88h+var_5D]
    movzx   eax, [rsp+88h+var_5E]
    movzx   eax, [rsp+88h+var_5F]
    movzx   eax, [rsp+88h+var_60]
    movzx   eax, [rsp+88h+var_61]
    movzx   eax, [rsp+88h+var_62]
    movzx   eax, [rsp+88h+var_63]
    movzx   eax, [rsp+88h+var_64]
    movzx   eax, [rsp+88h+var_65]
    movzx   eax, [rsp+88h+var_66]
    movzx   eax, [rsp+88h+var_67]
    movzx   eax, [rsp+88h+var_68]
    movzx   eax, [rsp+88h+var_69]
    movzx   eax, [rsp+88h+var_6A]
    movzx   eax, [rsp+88h+var_6B]
    movzx   eax, [rsp+88h+var_6C]
    movzx   eax, [rsp+88h+var_6D]
    movzx   eax, [rsp+88h+var_6E]
    movzx   eax, [rsp+88h+var_6F]
    movzx   eax, [rsp+88h+var_70]
    movzx   eax, [rsp+88h+var_71]
    movzx   eax, [rsp+88h+var_72]
    movzx   eax, [rsp+88h+var_73]
    movzx   eax, [rsp+88h+var_74]
    movzx   eax, [rsp+88h+var_75]
    movzx   eax, [rsp+88h+var_76]
    movzx   eax, [rsp+88h+var_77]
    movzx   eax, [rsp+88h+var_78]
    movzx   eax, [rsp+88h+var_79]
    movzx   eax, [rsp+88h+var_7A]
    movzx   eax, [rsp+88h+var_7B]
    movzx   eax, [rsp+88h+var_7C]
    movzx   eax, [rsp+88h+var_7D]
    movzx   eax, [rsp+88h+var_7E]
    movzx   eax, [rsp+88h+var_7F]
    movzx   eax, [rsp+88h+var_80]
    movzx   eax, [rsp+88h+var_81]
    movzx   eax, [rsp+88h+var_82]
    movzx   eax, [rsp+88h+var_83]
    movzx   eax, [rsp+88h+var_84]
    movzx   eax, [rsp+88h+var_85]
    movzx   eax, [rsp+88h+var_86]
    movzx   eax, [rsp+88h+var_88]
    movzx   eax, [rsp+88h+var_87]
    movzx   eax, [rsp+88h+var_48]
    dec     rbp
    jnz     loc_6C190
    
    Benchmarking long XOF outputs might be annoying.

--- benchmarks/benches/bench.rs
+++ benchmarks/benches/bench.rs
@@ -1,13 +1,116 @@
-use criterion::{criterion_group, criterion_main, Criterion, ParameterizedBenchmark, Throughput};
+use criterion::{criterion_group, criterion_main, Criterion, ParameterizedBenchmark, Throughput, measurement::{Measurement, ValueFormatter}};
 use rand::prelude::*;
 
 const MIN_LEN: usize = 64;
 const MAX_LEN: usize = 1 << 20;
 
-criterion_group!(cg, benchmarks);
+/* copy-pasted from https://docs.rs/crate/criterion-cycles-per-byte/0.1.1/source/src/lib.rs */
+
+/// `CyclesPerByte` measures clock cycles using the x86 or x86_64 `rdtsc` instruction. `cpb` is
+/// the preferrerd measurement for cryptographic algorithms.
+pub struct CyclesPerByte;
+
+// WARN: does not check for the cpu feature; but we'd panic anyway so...
+fn rdtsc() -> u64 {
+    #[cfg(target_arch = "x86_64")]
+    unsafe {
+        // core::arch::x86_64::_rdtsc()
+        let mut discard: u32 = 0;
+        core::arch::x86_64::__rdtscp(&mut discard)
+    }
+
+    #[cfg(target_arch = "x86")]
+    unsafe {
+        let mut discard: u32 = 0;
+        core::arch::x86::__rdtscp(&mut discard)
+        // core::arch::x86::_rdtsc()
+    }
+}
+
+impl Measurement for CyclesPerByte {
+    type Intermediate = u64;
+    type Value = u64;
+
+    fn start(&self) -> Self::Intermediate {
+        rdtsc()
+    }
+
+    fn end(&self, i: Self::Intermediate) -> Self::Value {
+        rdtsc() - i
+    }
+
+    fn add(&self, v1: &Self::Value, v2: &Self::Value) -> Self::Value {
+        v1 + v2
+    }
+
+    fn zero(&self) -> Self::Value {
+        0
+    }
+
+    fn to_f64(&self, value: &Self::Value) -> f64 {
+        *value as f64
+    }
+
+    fn formatter(&self) -> &dyn ValueFormatter {
+        &CyclesPerByteFormatter
+    }
+}
+
+struct CyclesPerByteFormatter;
+
+impl ValueFormatter for CyclesPerByteFormatter {
+    fn format_value(&self, value: f64) -> String {
+        format!("{:.4} cycles", value)
+    }
+
+    fn format_throughput(&self, throughput: &Throughput, value: f64) -> String {
+        match throughput {
+            Throughput::Bytes(b) => format!("{:.4} cpb", value / *b as f64),
+            Throughput::Elements(b) => format!("{:.4} cycles/{}", value, b),
+        }
+    }
+
+    fn scale_values(&self, _typical_value: f64, _values: &mut [f64]) -> &'static str {
+        "cycles"
+    }
+
+    fn scale_throughputs(
+        &self,
+        _typical_value: f64,
+        throughput: &Throughput,
+        values: &mut [f64],
+    ) -> &'static str {
+        match throughput {
+            Throughput::Bytes(n) => {
+                for val in values {
+                    *val /= *n as f64;
+                }
+                "cpb"
+            }
+            Throughput::Elements(n) => {
+                for val in values {
+                    *val /= *n as f64;
+                }
+                "c/e"
+            }
+        }
+    }
+
+    fn scale_for_machines(&self, _values: &mut [f64]) -> &'static str {
+        "cycles"
+    }
+}
+
+/* /copy-pasted from https://docs.rs/crate/criterion-cycles-per-byte/0.1.1/source/src/lib.rs */
+
+criterion_group!(
+    name = cg; 
+    config = Criterion::default().with_measurement(CyclesPerByte); 
+    targets = benchmarks
+);
 criterion_main!(cg);
 
-fn benchmarks(c: &mut Criterion) {
+fn benchmarks(c: &mut Criterion<CyclesPerByte>) {
     let mut params = Vec::new();
     let mut len = MIN_LEN;
     while len <= MAX_LEN {
